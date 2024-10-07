c2c44eb50061292f4edb523c0aeb3ecd8f04b52f
HugoGH1 <hectorglez758@hotmail.com>
2024-10-07 17:23:48 -0600

    Archivo login

---
+++ Lab2_M3.py
@@ -0,0 +1,7 @@
+entrada=input("Escribe el nombre de la planta: ")
+
+if entrada=="ESPATIFILIO":
+    print("Si, ¡El ESPATIFILIO! es la mejore planta de todos los tiempos")
+elif entrada=="espatifilo":
+    print("No, ¡quiero un gran ESPATIFILIO!")
+else:print("¡ESPATIFILIO!, ¡No",entrada,"!")
